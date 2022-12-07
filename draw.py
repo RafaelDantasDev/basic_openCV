8b3535acdf8fe372d17d10c5afa53a1317adc7de
Rafael Dantas <dantas.rafael8928@gmail.com>
2022-12-07 11:10:11 -0300

    Improving the design code

--- draw.py
+++ draw.py
@@ -9,16 +9,16 @@ cv.imshow('Blank', blank)
 #cv.imshow('Green', blank)
 
 # 2. Draw a Rectangle
-#cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
-#cv.imshow('Rectangle', blank)
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.imshow('Rectangle', blank)
 
 # 3. Draw a Circle
-#cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
-#cv.imshow('Circle', blank)
+cv.circle(blank, (blank.shape[1]//2, blank.shape[0]//2), 40, (0,0,255), thickness=-1)
+cv.imshow('Circle', blank)
 
 # 4. Draw a Line
-#cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
-#cv.imshow('Line', blank)
+cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
+cv.imshow('Line', blank)
 
 # 5. Write Text
 cv.putText(blank, 'Hello', (255,255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
